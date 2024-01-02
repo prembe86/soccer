9ce3fca498ecaf1d65a9fc4b2890d7143e0a0759
prembe86 <prembe86@gmail.com>
2024-01-02 12:36:42 -0600

    installing bootstrap and title change

--- src/app/interceptors/header.interceptor.ts
+++ src/app/interceptors/header.interceptor.ts
@@ -5,7 +5,7 @@ import { inject } from '@angular/core';
 export const headerInterceptor: HttpInterceptorFn = (req, next) => {
   const headers = new HttpHeaders()
     .set('x-rapidapi-host', 'v3.football.api-sports.io')
-    .set('x-rapidapi-key', 'f1d7b9bc3d7306b05020dc80b5881197');
+    .set('x-rapidapi-key', 'df6a5ce01417f57fa1ef9af581bb0db1');
   const authReq = req.clone({
     headers,
   });
